0b5129c002024d8ecb84c97246e30608d1957713
root <codingchild@gmail.com>
2021-12-30 01:17:05 +0000

    add autoword

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from json_parser import json_loader, json_parser_to_pd
 from xAPI_Analyzer import Analyzer
 import matplotlib.pyplot as plt
 from visualizer import act_visualizer, watching_visualizer, personal_watching_visualizer
+from auto_word import make_LA_report
 
 #argument_parser로 변경하기
 data_path = "data/XAPI_npe_1215.json"
@@ -28,3 +29,10 @@ watching_visualizer(unique_actor, data_list)
 for stu in unique_actor:
     verb_list = xAPI_analyzer.personal_watching_time(stu)
     personal_watching_visualizer(verb_list, stu)
+
+picture1 = 'visualization/act_visualization.png'
+picture2 = 'visualization/watching_visualization.png'
+#이건 사용자에 맞게 적절하게 수정해서 사용
+picture3 = 'visualization/personal_watching_visualization.png'
+
+make_LA_report(picture1, picture2, picture3)
